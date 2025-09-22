260859a7c945718c273c6ef3df56942b79d30743
iirufan <42711386+iirufan@users.noreply.github.com>
2025-09-22 10:18:43 +0500

    Create user.js

---
+++ user.js
@@ -0,0 +1,5 @@
+// user.js
+const users = [
+  { username: "Leeli", name: "Leeli Lounge", password: "Welcome@1234", rcNo: "1", WP: "Leeli", Level: "Supervisor" },
+  { username: "Koveli", name: "Koveli Lounge", password: "12345", rcNo: "2", WP: "Koveli", Level: "User" }
+];
